0298e7ea90265b2a1fbb3c81a6bb1fbe6fedd976
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-11-07 14:20:13 +0000

    Add typing effect using JS

--- assets/script.js
+++ assets/script.js
@@ -49,3 +49,55 @@ profileCard.addEventListener("click", ()=>{
     console.log("click")
     profileCard.classList.toggle("profile-card-click")
 })
+
+
+// --------- TYPING EFFECT --------------------------------
+
+let words = ['Love', 'Life', 'Art', 'Everything']
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false
+let typingSpan = document.querySelector(".typing-span")
+
+const typingEffect = () => {
+
+    let currentWord = words[wordIndex]
+    let currentCharacter = currentWord.substring(0, charIndex)
+    typingSpan.innerText = currentCharacter;
+    typingSpan.classList.add("stop-blink");
+
+    if(!isDeleting && charIndex < currentWord.length){
+
+        // If condition is true, type the next character
+
+        // console.log(currentCharacter)
+        charIndex++;
+        setTimeout(typingEffect, 200)
+
+    }
+
+    else if(isDeleting && charIndex > 0){
+
+        // If condition is true, remove the previous character
+
+        // console.log(currentCharacter)
+        charIndex--;
+        setTimeout(typingEffect, 100)
+
+    }
+
+    else{
+
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        typingSpan.classList.remove("stop-blink")
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex
+
+        setTimeout(typingEffect, 1200);
+
+    }
+
+
+}
+
+typingEffect()
